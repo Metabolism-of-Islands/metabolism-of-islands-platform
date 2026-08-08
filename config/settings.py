63123e7b710555948ab3f0d6e8a1361f3366d3db
Paul Hoekman <p@ulhoekman.com>
2026-08-08 18:03:34 -0500

    Set up static root

--- config/settings.py
+++ config/settings.py
@@ -124,6 +124,7 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
+STATIC_ROOT = os.environ['STATIC_ROOT']
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
